410da89a229023f0043c8fe8e4b4c1d561981699
Shengwen Yu <yushengwen@tron.network>
2018-12-26 14:24:54 +0800

    add more example for "for loop"

--- 07_loops/main.go
+++ 07_loops/main.go
@@ -1,8 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
 
 func main() {
+
+}
+
+func FizzBuzz() {
 	for i := 1; i <= 10; i++ {
 		fmt.Printf("number = %+v\n", i)
 	}
@@ -18,5 +27,28 @@ func main() {
 		} else {
 			fmt.Println(i)
 		}
+	}
+}
+
+// omit initial condition
+func convertToBin(n int) string {
+	result := ""
+	for ; n > 0; n /= 2 {
+		lsb := n % 2
+		result = strconv.Itoa(lsb) + result
+	}
+	return result
+}
+
+// omit initial condition and update condition
+func printFile(filename string) {
+	file, err := os.Open(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
 	}
 }
